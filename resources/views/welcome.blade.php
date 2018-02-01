cc994661709fdbc980f39bb686215aa98c15f3e6
zapahlevie <zapahlevie@gmail.com>
2018-02-01 15:25:06 +0700

    layout improvement

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,87 +1,304 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Dashboard</title>
-        <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
-        <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
-        <link href="{{asset('css/main.css')}}" rel="stylesheet" type="text/css">
-        <script src="{{asset('js/jquery.min.js')}}"></script>
-        <script src="{{asset('js/bootstrap.min.js')}}"></script>
-    </head>
-    <body id="page-top">
-      <header class="masthead">
-        <div class="container h-100">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Dashboard</title>
+  <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
+  <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
+  <link href="{{asset('css/main.css')}}" rel="stylesheet" type="text/css">
+  <script src="{{asset('js/jquery.min.js')}}"></script>
+  <script src="{{asset('js/bootstrap.min.js')}}"></script>
+</head>
+<body id="page-top">
+  <header class="masthead">
+    <div class="container h-100">
+    </div>
+  </header>
 
+  <section class="features" id="features">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-1">
+          <label>sort by:</label>
         </div>
-      </header>
-
-      <section class="features" id="features">
-        <div class="container">
-
-          <div class="row">
-              <div class="container-fluid">
-                <div class="row">
-                  <div class="col-lg-3">
-                    <div class="feature-item">
-                      <img src="/images/1.png"/>
-                    </div>
-                  </div>
-                  <div class="col-lg-3">
-                    <div class="feature-item">
-                      <img src="/images/1.png"/>
-                    </div>
-                  </div>
-                  <div class="col-lg-3">
-                    <div class="feature-item">
-                      <img src="/images/1.png"/>
-                    </div>
-                  </div>
-                  <div class="col-lg-3">
-                    <div class="feature-item">
-                      <img src="/images/1.png"/>
-                    </div>
-                  </div>
-                </div>
+        <div class="col-lg-4">
+          <form>
+            <div class="form-group">
+              <select class="form-control">
+                <option>Januari 2018</option>
+                <option>Desember 2017</option>
+                <option>November 2017</option>
+                <option>Oktober 2017</option>
+              </select>
             </div>
+          </form>
+        </div>
+        <div class="col-lg-4">
+          <form>
+            <div class="form-group">
+              <select class="form-control">
+                <option>Most Follower Growth</option>
+                <option>Highest Engagement Rate</option>
+              </select>
+            </div>
+          </form>
+        </div>
+        <div class="col-lg-2">
+          <div class="btn-group">
+            <button type="button" class="btn btn-primary" autofocus="autofocus"><i class="fa fa-facebook"></i></button>
+            <button type="button" class="btn btn-primary"><i class="fa fa-twitter"></i></button>
+            <button type="button" class="btn btn-primary"><i class="fa fa-youtube"></i></button>
+            <button type="button" class="btn btn-primary"><i class="fa fa-instagram"></i></button>
           </div>
         </div>
-      </section>
-
-      <section class="contact bg-primary" id="contact">
-        <div class="container">
-          <ul class="list-inline list-social">
-            <li class="list-inline-item social-facebook">
-              <a href="#">
-                <i class="fa fa-facebook"></i>
-              </a>
-            </li>
-            <li class="list-inline-item social-twitter">
-              <a href="#">
-                <i class="fa fa-twitter"></i>
-              </a>
-            </li>
-            <li class="list-inline-item social-google-plus">
-              <a href="#">
-                <i class="fa fa-youtube"></i>
-              </a>
-            </li>
-            <li class="list-inline-item social-google-plus">
-              <a href="#">
-                <i class="fa fa-instagram"></i>
-              </a>
-            </li>
-          </ul>
+        <div class="col-lg-1"></div>
+      </div>
+      <div class="row">
+        <div class="col-lg-3">
+          <div class="feature-item centerBlock">
+            <img src="/images/1.png"/>
+          </div>
         </div>
-      </section>
-
-      <footer>
-        <div class="container">
-          <p>Copyright &copy; 2017 indsight. All screenshoots &copy; their respective owners</p>
+        <div class="col-lg-3">
+          <div class="feature-item centerBlock">
+            <img src="/images/2.png"/>
+          </div>
         </div>
-      </footer>
-        <!-- <script src="{{asset('js/app.js')}}" ></script> -->
-    </body>
+        <div class="col-lg-3">
+          <div class="feature-item centerBlock">
+            <img src="/images/3.png"/>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="feature-item centerBlock">
+            <img src="/images/4.png"/>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div class="row product1">
+        <div class="col-sm-1">
+          <img src="/images/logo.jpg"/>
+        </div>
+        <div class="col-sm-2">
+          <span class="product-title">Panasonic</span><br>
+          <span class="product-growth">17.81%</span>
+          <span class="product-amount">11.2k</span>
+        </div>
+        <div class="col-sm-1">
+          <img src="/images/logo.jpg"/>
+        </div>
+        <div class="col-sm-2">
+          <span class="product-title">Panasonic</span><br>
+          <span class="product-growth">17.81%</span>
+          <span class="product-amount">11.2k</span>
+        </div><div class="col-sm-1">
+          <img src="/images/logo.jpg"/>
+        </div>
+        <div class="col-sm-2">
+          <span class="product-title">Panasonic</span><br>
+          <span class="product-growth">17.81%</span>
+          <span class="product-amount">11.2k</span>
+        </div><div class="col-sm-1">
+          <img src="/images/logo.jpg"/>
+        </div>
+        <div class="col-sm-2">
+          <span class="product-title">Panasonic</span><br>
+          <span class="product-growth">17.81%</span>
+          <span class="product-amount">11.2k</span>
+        </div>
+      </div>
+
+
+      <div class="row product">
+        <div class="col-sm-1">
+          <img src="/images/logo.jpg"/>
+        </div>
+        <div class="col-sm-2">
+          <span class="product-title">Panasonic</span><br>
+          <span class="product-growth">17.81%</span>
+          <span class="product-amount">11.2k</span>
+        </div>
+        <div class="col-sm-1">
+          <img src="/images/logo.jpg"/>
+        </div>
+        <div class="col-sm-2">
+          <span class="product-title">Panasonic</span><br>
+          <span class="product-growth">17.81%</span>
+          <span class="product-amount">11.2k</span>
+        </div><div class="col-sm-1">
+          <img src="/images/logo.jpg"/>
+        </div>
+        <div class="col-sm-2">
+          <span class="product-title">Panasonic</span><br>
+          <span class="product-growth">17.81%</span>
+          <span class="product-amount">11.2k</span>
+        </div><div class="col-sm-1">
+          <img src="/images/logo.jpg"/>
+        </div>
+        <div class="col-sm-2">
+          <span class="product-title">Panasonic</span><br>
+          <span class="product-growth">17.81%</span>
+          <span class="product-amount">11.2k</span>
+        </div>
+      </div>
+
+
+      <div class="row product">
+        <div class="col-sm-1">
+          <img src="/images/logo.jpg"/>
+        </div>
+        <div class="col-sm-2">
+          <span class="product-title">Panasonic</span><br>
+          <span class="product-growth">17.81%</span>
+          <span class="product-amount">11.2k</span>
+        </div>
+        <div class="col-sm-1">
+          <img src="/images/logo.jpg"/>
+        </div>
+        <div class="col-sm-2">
+          <span class="product-title">Panasonic</span><br>
+          <span class="product-growth">17.81%</span>
+          <span class="product-amount">11.2k</span>
+        </div><div class="col-sm-1">
+          <img src="/images/logo.jpg"/>
+        </div>
+        <div class="col-sm-2">
+          <span class="product-title">Panasonic</span><br>
+          <span class="product-growth">17.81%</span>
+          <span class="product-amount">11.2k</span>
+        </div><div class="col-sm-1">
+          <img src="/images/logo.jpg"/>
+        </div>
+        <div class="col-sm-2">
+          <span class="product-title">Panasonic</span><br>
+          <span class="product-growth">17.81%</span>
+          <span class="product-amount">11.2k</span>
+        </div>
+      </div>
+
+
+      <div class="row product">
+        <div class="col-sm-1">
+          <img src="/images/logo.jpg"/>
+        </div>
+        <div class="col-sm-2">
+          <span class="product-title">Panasonic</span><br>
+          <span class="product-growth">17.81%</span>
+          <span class="product-amount">11.2k</span>
+        </div>
+        <div class="col-sm-1">
+          <img src="/images/logo.jpg"/>
+        </div>
+        <div class="col-sm-2">
+          <span class="product-title">Panasonic</span><br>
+          <span class="product-growth">17.81%</span>
+          <span class="product-amount">11.2k</span>
+        </div><div class="col-sm-1">
+          <img src="/images/logo.jpg"/>
+        </div>
+        <div class="col-sm-2">
+          <span class="product-title">Panasonic</span><br>
+          <span class="product-growth">17.81%</span>
+          <span class="product-amount">11.2k</span>
+        </div><div class="col-sm-1">
+          <img src="/images/logo.jpg"/>
+        </div>
+        <div class="col-sm-2">
+          <span class="product-title">Panasonic</span><br>
+          <span class="product-growth">17.81%</span>
+          <span class="product-amount">11.2k</span>
+        </div>
+      </div>
+
+
+      <div class="row product">
+        <div class="col-sm-1">
+          <img src="/images/logo.jpg"/>
+        </div>
+        <div class="col-sm-2">
+          <span class="product-title">Panasonic</span><br>
+          <span class="product-growth">17.81%</span>
+          <span class="product-amount">11.2k</span>
+        </div>
+        <div class="col-sm-1">
+          <img src="/images/logo.jpg"/>
+        </div>
+        <div class="col-sm-2">
+          <span class="product-title">Panasonic</span><br>
+          <span class="product-growth">17.81%</span>
+          <span class="product-amount">11.2k</span>
+        </div><div class="col-sm-1">
+          <img src="/images/logo.jpg"/>
+        </div>
+        <div class="col-sm-2">
+          <span class="product-title">Panasonic</span><br>
+          <span class="product-growth">17.81%</span>
+          <span class="product-amount">11.2k</span>
+        </div><div class="col-sm-1">
+          <img src="/images/logo.jpg"/>
+        </div>
+        <div class="col-sm-2">
+          <span class="product-title">Panasonic</span><br>
+          <span class="product-growth">17.81%</span>
+          <span class="product-amount">11.2k</span>
+        </div>
+      </div>
+
+      <div class="row detail">
+        <div class="col-sm-3 text-center">
+            <button class="button">see more</button>
+        </div>
+        <div class="col-sm-3 text-center">
+          <button class="button">see more</button>
+        </div>
+        <div class="col-sm-3 text-center">
+          <button class="button">see more</button>
+        </div>
+        <div class="col-sm-3 text-center">
+          <button class="button">see more</button>
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+  <section class="contact bg-primary" id="contact">
+    <div class="container">
+      <ul class="list-inline list-social">
+        <li class="list-inline-item social-facebook">
+          <a href="#">
+            <i class="fa fa-facebook"></i>
+          </a>
+        </li>
+        <li class="list-inline-item social-twitter">
+          <a href="#">
+            <i class="fa fa-twitter"></i>
+          </a>
+        </li>
+        <li class="list-inline-item social-google-plus">
+          <a href="#">
+            <i class="fa fa-youtube"></i>
+          </a>
+        </li>
+        <li class="list-inline-item social-google-plus">
+          <a href="#">
+            <i class="fa fa-instagram"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </section>
+
+  <footer>
+    <div class="container">
+      <p>Copyright &copy; 2017 indsight. All screenshoots &copy; their respective owners</p>
+    </div>
+  </footer>
+  <!-- <script src="{{asset('js/app.js')}}" ></script> -->
+</body>
 </html>
